a4ddc883f64f677cf08549a1f1ce784fba417e0f
strobodov <m.kornegoor@atcomputing.nl>
2019-09-13 00:02:43 +0200

    Updated various comments, cleaned the script

--- script.sh
+++ script.sh
@@ -8,30 +8,39 @@
 source config/variables
 az login -u $azure -p $azure_password
 wait
-az group create -n "mooiman" --location "westeurope"
+#define name of the resource group and Azure region you want to deploy in. (see Azure documentation for current list of options)
+az group create -n "<<RESOURCE GROUP NAME>>" --location "<<AZURE REGION NAME"
 wait
 echo "Resource Group ready!"
-az vm create -n "testVMscript" -g "mooiman" --admin-username $admin --generate-ssh-keys --image ubuntults --verbose --public-ip-address-dns-name mooiman-testvm --size Standard_D4_v3
+
+#define what name you want to give the VM and DNS. Make sure these names are identical in the config/variables file!
+az vm create -n "<<VM NAME>>" -g "<<RESOURCE GROUP NAME" --admin-username $admin --generate-ssh-keys --image ubuntults --verbose --public-ip-address-dns-name <<DNS NAME>> --size Standard_D4_v3
 wait
 echo "Virtual Server ready!"
+
 #log into the server and install updates and various applications & packages
 ssh -i <PATH TO SSH KEY> -l "$admin" "$fqdn" -o StrictHostKeyChecking=no <<EOF
 
 #update server 
 sudo apt update && sudo apt upgrade --assume-yes --ignore-missing
 wait
+
 #install Docker
 sudo apt-get install docker.io --assume-yes
 wait
+
 #install Virtualbox
 sudo apt-get install virtualbox --assume-yes
 wait
+
 #install Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.3.1.deb \
  && sudo dpkg -i minikube_1.3.1.deb
 wait
+
 #set virtualbox as default virtual  host
 minikube config set vm-driver virtualbox
+
 #install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 minikube start
