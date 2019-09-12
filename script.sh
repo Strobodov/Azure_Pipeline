653fc510037af56cbd31b7113480b5093a24e2a5
Marcel Kornegoor <mkornegoor@localhost.localdomain>
2019-09-12 23:32:25 +0200

    Translation to English completed.

--- script.sh
+++ script.sh
@@ -2,41 +2,41 @@
 
 ####################################################################################
 #DISCLAIMER
-#Dit script is alleen geschikt voor test-omgevingen en niet voor productie! Er is geen rekening gehouden met diverse aspecten op gebied van beveiliging, redundantie of stabiliteit
+#This script should only be used for testing and NOT for any kind of production environment. There has been no focus on any subject regarding security, redundancy or reliability!
 ####################################################################################
 
-source ./Documents/passwords/passwords
+source config/variables
 az login -u $azure -p $azure_password
 wait
 az group create -n "mooiman" --location "westeurope"
 wait
-echo "De Resource Group staat klaar!"
+echo "Resource Group ready!"
 az vm create -n "testVMscript" -g "mooiman" --admin-username $admin --generate-ssh-keys --image ubuntults --verbose --public-ip-address-dns-name mooiman-testvm --size Standard_D4_v3
 wait
-echo "De VM staat klaar!"
-#log in op de VM en installeer alle CI-CD pakketten
-ssh -i /home/mkornegoor/.ssh/id_rsa -l "$admin" "$fqdn" -o StrictHostKeyChecking=no <<EOF
+echo "Virtual Server ready!"
+#log into the server and install updates and various applications & packages
+ssh -i <PATH TO SSH KEY> -l "$admin" "$fqdn" -o StrictHostKeyChecking=no <<EOF
 
-#update de server 
+#update server 
 sudo apt update && sudo apt upgrade --assume-yes --ignore-missing
 wait
-#installeer Docker
+#install Docker
 sudo apt-get install docker.io --assume-yes
 wait
-#installeer Virtualbox
+#install Virtualbox
 sudo apt-get install virtualbox --assume-yes
 wait
-#installeer minikube
+#install Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.3.1.deb \
  && sudo dpkg -i minikube_1.3.1.deb
 wait
-#stel virtualbox in als standaard host
+#set virtualbox as default virtual  host
 minikube config set vm-driver virtualbox
-#installeer kubectl
+#install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 minikube start
 
-#installeer Helm en start tevens Tiller
+#install Helm and start Tiller
 sudo snap install helm --classic
 helm init --history-max 200
 EOF
